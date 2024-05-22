ba95a4d9da10a4abd5c52d1e139f60a172735431
rathinashabur <rathina.shabur@ui.ac.id>
2024-05-23 00:10:16 +0700

    gold challenge

---
+++ app.py
@@ -0,0 +1,98 @@
+import re
+import pandas as pd
+
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+from flask import request
+from flasgger import Swagger, LazyString, LazyJSONEncoder
+from flasgger import swag_from
+
+app.json_encoder = LazyJSONEncoder
+swagger_template = dict(
+info = {
+    'title': LazyString(lambda: 'API Documentation for Data Processing and Modeling'),
+    'version': LazyString(lambda: '1.0.0'),
+    'description': LazyString(lambda: 'Dokumentasi API untuk Data Processing dan Modeling'),
+    },
+    host = LazyString(lambda: request.host)
+)
+swagger_config = {
+    "headers": [],
+    "specs": [
+        {
+            "endpoint": 'docs',
+            "route": '/docs.json',
+        }
+    ],
+    "static_url_path": "/flasgger_static",
+    "swagger_ui": True,
+    "specs_route": "/docs/"
+}
+swagger = Swagger(app, template=swagger_template,             
+                  config=swagger_config)
+
+conn = sqlite3.connect('database.db', check_same_thread=False)
+
+df_new_kamusalay = pd.read_sql_query('SELECT * FROM newKamusAlay', conn)
+df_abusive = pd.read_sql_query('SELECT * FROM abusive', conn)
+
+
+@swag_from("docs/hello_world.yml", methods=['GET'])
+@app.route('/', methods=['GET'])
+def hello_world():
+    json_response = {
+        'status_code': 200,
+        'description': "Menyapa Hello World",
+        'data': "Hello World",
+    }
+
+    response_data = jsonify(json_response)
+    return response_data
+
+@swag_from("docs/text_processing.yml", methods=['POST'])
+@app.route('/text-processing', methods=['POST'])
+def text_processing():
+
+    text = request.form.get('text')
+
+    json_response = {
+        'status_code': 200,
+        'description': "Teks yang sudah diproses",
+        'data': re.sub(r'[^a-zA-Z0-9]', ' ', text),
+    }
+
+    response_data = jsonify(json_response)
+    return response_data
+
+@swag_from("docs/text_processing_file.yml", methods=['POST'])
+@app.route('/text-processing-file', methods=['POST'])
+def text_processing_file():
+
+    # Upladed file
+    file = request.files.getlist('file')[0]
+
+    # Import file csv ke Pandas
+    df = pd.read_csv(file)
+
+    # Ambil teks yang akan diproses dalam format list
+    texts = df.text.to_list()
+
+    # Lakukan cleansing pada teks
+    cleaned_text = []
+    for text in texts:
+        cleaned_text.append(re.sub(r'[^a-zA-Z0-9]', ' ', text))
+
+    json_response = {
+        'status_code': 200,
+        'description': "Teks yang sudah diproses",
+        'data': cleaned_text,
+    }
+
+    response_data = jsonify(json_response)
+    return response_data
+
+if __name__ == '__main__':
+   app.run()
+
